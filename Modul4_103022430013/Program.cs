ca2f4284886035042d447772298c0ddcf4b5632b
Dwiriz12 <dwirizkiindriyanti12@gmail.com>
2026-03-28 08:43:37 +0700

    second commit

--- Modul4_103022430013/Program.cs
+++ Modul4_103022430013/Program.cs
@@ -1,1 +1,42 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+public class KodePaket
+{
+    string[] namePaket =
+    {
+        "Basic", "Standart", "Premium", "Unlimited", "Gaming",
+        "Streaming", "Family", "Business", "Student", "Traveler"
+    };
+
+    string[] kodePaket =
+    {
+        "P201", "P202", "P203", "P204", "P205", "P206",
+        "P207", "P208", "P209", "P210",
+    };
+
+    public string getKodePaket(string namaPaket)
+    {
+
+        for (int i = 0; i < namaPaket.Length; i++)
+        {
+            if (namePaket[i] == namaPaket)
+            {
+                return kodePaket[i];
+            }
+        }
+        return "Kode tidak ditemukan";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KodePaket kode = new KodePaket();
+
+            Console.Write("Masukkan nama paket: ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine(kode.getKodePaket(input));
+
+        }
+    }
+}
