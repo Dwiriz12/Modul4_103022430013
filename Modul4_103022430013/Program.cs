6a625e7501bdd8ca9e545afd642844b575c5d869
Dwiriz12 <dwirizkiindriyanti12@gmail.com>
2026-03-28 09:16:45 +0700

    “menambahkan class MesinKopi”

--- Modul4_103022430013/Program.cs
+++ Modul4_103022430013/Program.cs
@@ -26,6 +26,57 @@ public class KodePaket
         return "Kode tidak ditemukan";
     }
 
+    class mesinKopi
+    {
+        enum state
+        {
+            OFF, STANBY, BREWING, MAINTENANCE
+        }
+        private state currentState;
+
+        public mesinKopi()
+        {
+            currentState = state.OFF;
+        }
+
+        public void ChangeState (state newState)
+        {
+            if (currentState == state.OFF && newState == mesinKopi.state.STANDBY)
+            {
+                Console.WriteLine("Mesin off berubah menjadi Standby");
+            }
+
+            else if (currentState == state.STANDBY && newState == mesinKopi.state.BREWING)
+            {
+                Console.WriteLine("Mesin Standby berubah menjadi Brewing");
+            }
+
+            else if (currentState == state.BREWING && newState == mesinKopi.state.STANDBY)
+            {
+                Console.WriteLine("Mesin Brewing berubah menjadi Standby");
+            }
+
+            else if (currentState == state.STANDBY && newState == mesinKopi.state.MAINTENANCE)
+            {
+                Console.WriteLine("Mesin Standby berubah menjadi Maintenance");
+            }
+
+            else if (currentState == state.MAINTENANCE && newState == mesinKopi.state.STANDBY)
+            {
+                Console.WriteLine("Mesin Maintenance berubah menjadi Standby");
+            }
+
+            else if (currentState == state.STANDBY && newState == mesinKopi.state.OFF)
+            {
+                Console.WriteLine("Mesin Standby berubah menjadi Off");
+            }
+
+            else {  
+                Console.WriteLine("Perubahan state tidak valid");
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -34,9 +85,18 @@ public class KodePaket
 
             Console.Write("Masukkan nama paket: ");
             string input = Console.ReadLine();
-
+            jjk
             Console.WriteLine(kode.getKodePaket(input));
+            
 
+            mesinKopi.OFF();
+            mesinKopi.STANBY();
+            mesinKopi.BREWING();
+            mesinKopi.STANDBY();
+            mesinKopi.MAINTENANCE();
+            mesinKopi.STANBY();
+
+            mesinKopi.BREWING();
         }
     }
 }
